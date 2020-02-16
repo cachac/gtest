11b1bd850148cabdb4d92e55aa70593dc8b92d4f
cachac <carlos.echc11@gmail.com>
2020-02-16 14:45:50 -0600

    main

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 const index = require('./index')
 // const compression = require('compression');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 
 const app = express()
 // app.use(compression())
-app.use(helmet())
+// app.use(helmet())
 app.use(cors())
 app.use(index)
 
