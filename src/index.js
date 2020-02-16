2267a0a696a68c8618cbfb28b7b9ca6a7db3fb98
cachac <carlos.echc11@gmail.com>
2020-02-16 13:18:05 -0600

    express

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ const express = require ('express')
 
 const router = express.Router()
 
-router.get('/info', (req, res) => res.send({ app: process.env.APP_NAME, env: process.env.NODE_ENV, port: process.env.NODE_PORT, version: process.env.APP_VERSION }))
-router.post('/info', (req, res) => res.send({ app: process.env.APP_NAME, env: process.env.NODE_ENV, port: process.env.NODE_PORT, version: process.env.APP_VERSION }))
+router.get('/info', (req, res) => {
+  console.log('> get info');
+  res.send({ app: process.env.APP_NAME, env: process.env.NODE_ENV, port: process.env.NODE_PORT, version: process.env.APP_VERSION })
+})
+router.post('/info', (req, res) => {
+  console.log('> post info');
+  res.send({ app: process.env.APP_NAME, env: process.env.NODE_ENV, port: process.env.NODE_PORT, version: process.env.APP_VERSION })
+})
 
 router.get('*', function(req, res) {
   res.status(404).send('Página no encontrada :/')
--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ const compression = require('compression');
 const helmet = require('helmet');
 
 const app = express()
-app.use(compression)
-app.use(helmet)
+app.use(compression())
+app.use(helmet())
 app.use(cors())
 app.use(index)
 
 dotenv.config()
 
 app.listen(process.env.NODE_PORT, () => {
-  console.log(`[${process.env.NODE_ENV}] App: ${process.env.APP_NAME} v${process.env.APP_VERSION} listening to port ${process.env.NODE_PORT} - Express JS | REST`)  
+  console.log(`[${process.env.NODE_ENV}] App: ${process.env.APP_NAME} v${process.env.APP_VERSION} listening to port ${process.env.NODE_PORT} - Express JS`)
 })
