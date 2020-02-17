1282d38c1585e928737d35359b8f1bc6223a67dc
cachac <carlos.echc11@gmail.com>
2020-02-16 19:31:12 -0600

    yaml

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require ('express')
 
 const router = express.Router()
 
-const custom = 'yaml!0.3'
+const custom = 'yaml! listo para dos ambientes'
 
 router.get('/info', (req, res) => {
   console.log('> get info');
